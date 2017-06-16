991d5e369ffc8399da5b9ab984769ee1a2166794
David Tudury <david.tudury@gmail.com>
2017-06-15 22:59:17 -0700

    wip

--- test/Striem.test.js
+++
@@ -1,8 +0,0 @@
-
-const Striem = require("../lib/Striem");
-
-let s = Striem({b:4});
-
-s.x = 5;
-console.log(s);
-console.log("s.x:", s.x);
--- test/hash.test.js
+++ test/hash.test.js
@@ -1,13 +1,12 @@
 const {hash, manage, insert} = require('../lib/hash');
 
-function test (v) {
-    console.log(hash(v))
+function test(v) {
+    hash(v, [], h => console.log(h, v))
 }
 
 class Test {
-    constructor () {}
+    constructor() {}
 }
-
 
 test(null);
 test(true);
@@ -22,18 +21,16 @@ test("asdf");
 test("null");
 test("true");
 test("1234");
-test([1,2,3]);
-test([3,2,1]);
-test({a:1, b:2});
-test({b:2, a:1});
-let o = {a:1};
+test([1, 2, 3]);
+test([3, 2, 1]);
+test({a: 1, b: 2});
+test({b: 2, a: 1});
+let o = { a: 1 };
 o.o = o;
 test(o);
-let p = {a:1};
+let p = { a: 1 };
 p.o = p;
 test(p);
-let q = {o, p};
+let q = { o, p };
 test(q);
 test(new Test)
-
-manage("asdf")
